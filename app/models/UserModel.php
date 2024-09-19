5d059aafd619582410bd3b6fb2655af24f276f15
Ahmad Fauzan <noxindocraft@gmail.com>
2024-09-19 21:15:05 +0700

    added example of model usage

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -4,4 +4,18 @@ class IndexModel extends Model{
     protected $table = 'users';
     protected $primaryKey = 'userId';
 
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function getUserById(int $id): array
+    {
+        $this->db->query("SELECT * FROM $this->table WHERE $this->primaryKey = :value");
+        $this->db->bind(':value', $id);
+        return $this->db->single();
+    }
+
 }
